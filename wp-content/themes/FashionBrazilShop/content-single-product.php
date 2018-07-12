ab11e1d110b0a38d77f897df0fe61f7fadd3f605
= <Garry@yeatman.co.nz>
2018-07-12 20:02:55 +1200

    Fixes and logo change

--- wp-content/themes/FashionBrazilShop/content-single-product.php
+++ wp-content/themes/FashionBrazilShop/content-single-product.php
@@ -76,7 +76,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                                         * @hooked woocommerce_template_single_meta - 40
                                         * @hooked woocommerce_template_single_sharing - 50
                                         */
-                                        do_action( 'woocommerce_single_product_summary' );
+                                        do_action('woocommerce_single_product_summary');
                                     ?>
                                 </div>
                             </div>
